867eb05f65a01ac8facb56b99baa94a7b09364f9
MiguelLopez14 <miguel.lopezle14@gmail.com>
2025-08-15 07:03:53 -0500

    Delete src directory

--- src/aspirantes/aspirantes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AspirantesService } from './aspirantes.service';
-import { AspirantesController } from './aspirantes.controller';
-
-@Module({
-  controllers: [AspirantesController],
-  providers: [AspirantesService],
-})
-export class AspirantesModule {}
